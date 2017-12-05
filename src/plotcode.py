c388555cf9baa94a4262f0a394b369d0dd69aed1
YashuNavetia <32020496+YashuNavetia@users.noreply.github.com>
2017-12-05 04:39:46 -0500

    Plot code version 2

--- src/plotcode.py
+++ src/plotcode.py
@@ -13,30 +13,74 @@ from sklearn.metrics import mean_squared_error, r2_score
 from sklearn.neighbors import KNeighborsClassifier
 import warnings
 warnings.filterwarnings('ignore')
-df = pd.read_csv('Zone1_2014_01.csv')
- 
-#ysample = random.sample(range(-50, 50), 100)
+def uniqueish_color():
+    """There're better ways to generate unique colors, but this isn't awful."""
+    return plt.cm.gist_ncar(np.random.random())
+#df = pd.read_csv('plot.csv')
+#%matplotlib inline
+import numpy as np
+import matplotlib.pyplot as plt
+from mpl_toolkits.basemap import Basemap
+
+
+#fig = plt.figure(figsize=(8, 8))
+#m = Basemap(projection='lcc', resolution=None,
+#            width=2E6, height=2E6, 
+#            lat_0=30, lon_0=-120,)
+#m.etopo(scale=0.5, alpha=0.5)
+df = pd.read_csv('actual55.csv')
+pf = pd.read_csv('predicted55.csv') 
+
 xdata = []
 ydata = [] 
-Xdata = df['latitude']
-Ydata = df['longitude']
-plt.show()
- 
-axes = plt.gca()
-axes.set_xlim(45, 55)
-axes.set_ylim(-180, -170)
-line, = axes.plot(xdata, ydata, 'r-')
-  
+Ydata = df['latitude']
+Xdata = df['longitude']
 
+pxdata =[]
+pydata =[]
+PYdata = pf['latitude']
+PXdata = pf['longitude']
+  
+print("hi")
+print (df['Timestamp'].count())
+plt.axes()
+plt.xlabel('longitude')
+plt.ylabel('latitude')
+plt.xlim([-125, -110])
+plt.ylim([25, 40])
+t=0.01
+for i in range(0,df['Timestamp'].count(),200):
+    #xdata = Xdata[i]
+
+    #ydata = Ydata[i]
+
+    pxdata,pydata = (PXdata[i],PYdata[i])
+    xdata,ydata = (Xdata[i],Ydata[i])
+    
+    #print (Xdata[i])
+    #print (Ydata[i])
+    #ydata = Ydata[i]
  
-for i in range(50000):
-    xdata.append(Xdata[i])
-    ydata.append(Ydata[i])
-    line.set_xdata(xdata)
-    line.set_ydata(ydata)
+    plt.plot(xdata,ydata, marker='*', markersize=5, color = 'red')#uniqueish_color())
+    plt.plot(pxdata,pydata, marker='*', markersize=5, color = 'black')
+    #plt.text(x, y, ' Seattle', fontsize=12);
+
     plt.draw()
     plt.pause(1e-17)
-    time.sleep(0.1)
+    #time.sleep(1)
+    print (i)
+    #print (pxdata)
+    #print (xdata)
+print("hello")
+ 
+# for i,f in df['Timestamp'].iteritems():
+#     xdata.append(Xdata[i])
+#     ydata.append(Ydata[i])
+#     line.set_xdata(xdata)
+#     line.set_ydata(ydata)
+#     plt.draw()
+#     plt.pause(1e-17)
+#     time.sleep(0.0000001)
  
 # add this if you don't want the window to disappear at the end
 plt.show()
